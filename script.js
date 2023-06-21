73de5eaf1484c6f2f729ba9c4947ceeb3c16dec1
Ömür <omurkkasap@gmail.com>
2023-06-21 17:39:28 +0300

    dark mode eklendi.

--- script.js
+++ script.js
@@ -31,14 +31,17 @@ let navClose = document.getElementById('nav-close');
 function showNav(){
   navMenu.style.display = 'flex';
   navigation.style.height = '320px';
+  navToggle.style.visibility = 'hidden';
+  navMenu.style.boxShadow = '0 0 0'; // görünen bozukluklar giderildi
 }
-
-
 
 function hideNav(){
   navMenu.style.display = 'none';
   navigation.style.height = '80px';
+  navToggle.style.visibility = 'visible';
 }
+
+
 
 
 // ------------------------------- HİDE NAV WHEN GO TO SECTİON FOR SMALLER DEVICES -------------------------------
@@ -71,3 +74,40 @@ function subscribe() {
 // ------------------------------------- SCROLL TO -------------------------------------
 // ------------------------------------- SCROLL REVEAL -------------------------------------
 // ------------------------------------- TOGGLE DAY/NIGHT MODE -------------------------------------
+let moon = document.getElementById('moon');
+let sun = document.getElementById('sun');
+let root = document.querySelector(':root');
+
+function changeToDark() {
+  moon.style.visibility = 'hidden';
+  moon.style.opacity = '0';
+  sun.style.visibility = 'visible';
+  sun.style.opacity = '1';
+
+  // Change Color
+  root.style.setProperty('--text-color', '#fff')
+  root.style.setProperty('--bg-color', '#1f1a45')
+  root.style.setProperty('--dark-color', '#aaf5ff');
+  root.style.setProperty('--header-change-color', '#193fa8d7');
+  root.style.setProperty('--hover-color', '#00bcd4');
+  root.style.setProperty('--bg-color-header', '#1f1a45d3');
+
+  // Change Banner 
+  document.getElementById('banner').src = 'assets/line2.png'
+}
+
+function changeToMain() {
+  moon.style.visibility = 'visible';
+  moon.style.opacity = '1';
+  sun.style.visibility = 'hidden';
+  sun.style.opacity = '0';
+
+  // Turn back Color
+  root.style.setProperty('--text-color', '#000')
+  root.style.setProperty('--bg-color', '#eaf5a5')
+  root.style.setProperty('--dark-color', '#010050');
+  root.style.setProperty('--header-change-color', '#e8eba6d7');
+  root.style.setProperty('--hover-color', '#049395');
+  root.style.setProperty('--bg-color-header', '#eaf5a5d3');
+  document.getElementById('banner').src = 'assets/line.png'
+}
